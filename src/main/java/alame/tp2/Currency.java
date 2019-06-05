756e306d0df1b4f28bb1030d2c320dfddfdf0d72
Ibrahim ALAME <ibrahimalame@iMac-de-Ibrahim.local>
2019-06-05 07:23:33 +0200

    deuxième commit

--- src/main/java/alame/tp2/Currency.java
+++ src/main/java/alame/tp2/Currency.java
@@ -1,11 +1,11 @@
 package alame.tp2;
+
 import java.text.NumberFormat;
-import java.util.Locale;
 
 /**
  * Currency.java<BR>
- * Construit une cha�ne format�e � partir d'un r�el repr�sentant une somme
- * dans une monnaie donn�e.
+ * Construit une chaîne formatée à partir d'un réel représentant une somme
+ * dans une monnaie donnée.
  *
  */
 public class Currency {
@@ -13,21 +13,29 @@ public class Currency {
     public static final String EURO = "" + (char) (8364);
     // le symbole pour les dollars US
     public static final String USD = "$US";
+    // le CAD
+    public static final String CAD = "$CA";
     // le symbole pour les Livres Sterling
-    public static final String POUNDS = "�";
-
-
+    public static final String POUNDS = "£";
+    // le Yen
+    public static final String YEN = "¥";
+    // le Roupie
+    public static final String ROUPIE = "₹";
+    // le Youan
+    public static final String YOUAN = "元";
+    // le CHF
+    public static final String CHF = "₣";
     /**
-     * formatte une valeur r�elle r�pr�sentant une somme dans une monnaie donn�e.
-     * Retourne la chaine repr�sentant le nombre r�el arrondi avec un nombre de
-     * d�cimales fix� suivant du symbole de la monnaie souhait�e.<BR>
+     * formatte une valeur réelle réprésentant une somme dans une monnaie donnée.
+     * Retourne la chaine représentant le nombre réel arrondi avec un nombre de
+     * décimales fixé suivant du symbole de la monnaie souhaitée.<BR>
      * Par exemple pour formmatter une somme en $. Si 10.4586 est le montant de
-     * la somme, la cha�ne format�e sera : "10.46 $".
+     * la somme, la chaîne formatée sera : "10.46 $".
      *
-     * @param v  la valeur � formatter
-     * @param nbdec le nombre de d�cimales souhait�es
-     * @param monnaieSymb La cha�ne repr�sentant le symbole de la monnaie
-     * @return la cha�ne repr�sentant la valeur formatt�e.
+     * @param v  la valeur à formatter
+     * @param nbdec le nombre de décimales souhaitées
+     * @param monnaieSymb La chaîne représentant le symbole de la monnaie
+     * @return la chaîne représentant la valeur formattée.
      */
     public static String formatte(double v, int nbdec, String monnaieSymb) {
         NumberFormat fmt = NumberFormat.getInstance();
--- src/main/java/alame/tp2/DialogTauxConversion.java
+++ src/main/java/alame/tp2/DialogTauxConversion.java
@@ -1,26 +1,23 @@
 package alame.tp2;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
-import alame.tp3.Taux;
-
-import javax.swing.JDialog;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
-import javax.swing.JTextField;
-import javax.swing.JButton;
 import java.awt.event.ActionEvent;
 import java.lang.NumberFormatException;
-import java.awt.FlowLayout;
-import java.awt.Container;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 
 // JDialog permettant de fixer le taux de conversion
 public class DialogTauxConversion extends JDialog implements ActionListener {
 
-    private Convertisseur conv;//l'objet convertisseur auquel est associ� ce Dialog
-    private JTextField taux; //le TextField pour la saisie du taux
-    private JButton btV; // le bouton pour la validation du taux
-    private JButton btA;// le bouton pour  annulation
+    private Convertisseur conv; //l'objet convertisseur auquel est associ� ce Dialog
+    private JTextField tfTaux;  //le TextField pour la saisie du taux
+    private JButton btV;  // le bouton pour la validation du taux
+    private JButton btA; // le bouton pour  annulation
+    private JButton btI; // le bouton internet
 
 
     //constructeur du dialogue pour fixer le taux.
@@ -32,26 +29,45 @@ public class DialogTauxConversion extends JDialog implements ActionListener {
 
         // mise en place des composants graphique du dialogue
         Container contentPane = this.getContentPane();
-        contentPane.setLayout(new FlowLayout());
-        contentPane.add(new JLabel("Taux de change"));
-        contentPane.add(taux = new JTextField(5));
-        taux.setText(""+ Taux.getValue());
-        contentPane.add(btV = new JButton("Valider"));
-        contentPane.add(btA = new JButton("Annuler"));
+        //contentPane.setLayout(new FlowLayout());
+        JPanel pNord = new JPanel();
+        JPanel pSud = new JPanel();
+        pNord.add(new JLabel("Taux de change"));
+        pNord.add(tfTaux = new JTextField(5));
+
+        pNord.add(btI=new JButton("Internet"));
+        tfTaux.setText(""+ conv.getTaux());
+        pSud.add(btV = new JButton("Valider"));
+        pSud.add(btA = new JButton("Annuler"));
+        contentPane.add(pNord, BorderLayout.NORTH);
+        contentPane.add(pSud, BorderLayout.SOUTH);
         btV.addActionListener(this);
         btA.addActionListener(this);
-
+        btI.addActionListener(this);
         this.setResizable(false);
         this.pack();
 
     }
+    public void downloadTaux(){
+        try{
+            Document doc = Jsoup.connect("http://www.boursorama.com/bourse/devises/taux-de-change-euro-dollar-EUR-USD").get();
+            Element div = doc.select("div.c-faceplate__info").first();
+            String t = div.select("span.c-instrument--last").text();
+            tfTaux.setText(t);// = Double.parseDouble(t);
+        }catch (Exception ex){
 
+        }
+
+    }
     // methode invoqu� lorsque l'on clique sur l'un des boutons
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == btV){
+        if (e.getSource() == btI){
+            downloadTaux();
+        }else if (e.getSource() == btV){
             try {
-                double val = Double.parseDouble(taux.getText());
-                conv.fixerTaux(val);
+                double val = Double.parseDouble(tfTaux.getText());
+                conv.setTaux(val);
+                conv.lblTaux.setText("1 euor = "+ Currency.formatte(val,5, Currency.USD));
                 this.setVisible(false);
             }
             catch (NumberFormatException ex) {
